7cbe9310343cf422b2c838282a5b8aea2d429ad8
Aryan <adriyanaryan@yahoo.com>
2025-08-13 23:18:07 +0600

    Create login.js

---
+++ bot/login/login.js
@@ -0,0 +1,14 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const login = () => {
+  const tokens = global.states.tokens;
+
+  const bots = tokens.map(token => new TelegramBot(token, { polling: true }));
+  const { listen } = require('../listen.js');
+
+  bots.forEach(bot => listen(bot));
+
+  return bots;
+};
+
+module.exports = { login };
