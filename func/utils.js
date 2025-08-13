feed84afcbcb93c92cfe2cc78e36517bb103c56b
Aryan <adriyanaryan@yahoo.com>
2025-08-14 00:03:49 +0600

    Update utils.js

--- func/utils.js
+++ func/utils.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
-const { create, clear } = require('./cache.js');
+const { create, clear } = require('../database/cache.js');
 
 const cacheReady = (async () => {
   await create();
