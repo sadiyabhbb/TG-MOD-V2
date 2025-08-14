153bbd71fa01eccac68121b4396c0ee849df83bb
Aryan <adriyanaryan@yahoo.com>
2025-08-14 18:15:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("TELEGRAM IS RUNNING 🟢"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dashboard", "index.html"));
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 require("./main.js");
