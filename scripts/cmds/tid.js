a120d9626b868926be5052d2c0aee68df6f3c3be
Aryan <adriyanaryan@yahoo.com>
2025-08-14 22:25:58 +0600

    Create tid.js

---
+++ scripts/cmds/tid.js
@@ -0,0 +1,20 @@
+module.exports = {
+  nix: {
+    name: "tid",
+    version: "1.0.0",
+    author: "ArYAN",
+    role: 0,
+    description: "Show current chat TID (group ID)",
+    category: "utility",
+    usage: "tid",
+    cooldown: 2,
+    prefix: true
+  },
+
+  async onStart({ bot, message, msg, chatId }) {
+    return message.reply(`• TID: \`${chatId}\``, {
+      parse_mode: "Markdown",
+      reply_to_message_id: msg.message_id
+    });
+  }
+};
