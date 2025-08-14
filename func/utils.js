e9d81fc377e8e53389b8d8fb5e517547ac8c0e0c
Aryan <adriyanaryan@yahoo.com>
2025-08-14 15:35:54 +0600

    Update utils.js

--- func/utils.js
+++ func/utils.js
@@ -1,41 +1,6 @@
-"use strict";
-
-const { execSync } = require("child_process");
 const fs = require('fs-extra');
 const path = require('path');
 const { create, clear } = require('../database/cache.js');
-
-const RESTART_CODE = 0;
-
-exports.install = function() {
-  const originalRequire = module.constructor.prototype.require;
-
-  module.constructor.prototype.require = function(moduleName) {
-    try {
-      return originalRequire.call(this, moduleName);
-    } catch (error) {
-      if (error.code === "MODULE_NOT_FOUND" && !moduleName.startsWith(".") && !moduleName.startsWith("/")) {
-        execSync(`npm install ${moduleName}`, {
-          stdio: "inherit",
-          cwd: process.cwd(),
-        });
-        restartBot();
-
-        return originalRequire.call(this, moduleName);
-      }
-      throw error;
-    }
-  };
-};
-
-function restartBot() {
-  process.exit(RESTART_CODE);
-}
-
-if (!global.install) {
-  exports.install();
-  global.install = true;
-}
 
 const cacheReady = (async () => {
   await create();
@@ -73,10 +38,12 @@ async function loadDirectory(directory, moduleType, collection) {
         validateModule(moduleExport);
         collection.set(moduleExport.nix.name, moduleExport);
       } catch (error) {
+        console.error(`Error loading ${moduleType} "${file}": ${error.message}`);
         errors[file] = error;
       }
     }
   } catch (error) {
+    console.error(`Error reading ${moduleType} directory "${directory}": ${error.message}`);
     errors.directory = error;
   }
 
